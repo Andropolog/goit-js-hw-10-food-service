f461f4a8dfddbace3376c11323f3e74d5310663b
Andrii Kyreikov <Andropolog@gmail.com>
2021-05-07 17:09:02 +0300

    test

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import './styles.css';
+// console.log(hi);
+import "./test";
+
+// import print from "./test";
+// console.log(print);
+// print("not hi")
+
+
+// import { text, printText } from "./test"
+// console.log(text);
+// printText("J - S");
+
+import * as object from "./test"
+console.log(object.text);
+object.printText("dghdhg");
+
